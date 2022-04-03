a4cdcb68f1dfb02a9b250154025f33884aab4577
Simply Loveall <max.hkre@gmail.com>
2022-04-03 11:19:00 -0400

    added image

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+window.onload = () => {
+  document.getElementById("").onclick = () => {
+    startGame();
+  };
+};
+
 const canvas = document.getElementById("the-ocean");
 const ctx = canvas.getContext("2d");
 canvas.width = 500;
@@ -5,12 +11,21 @@ canvas.height = 500;
 canvas.width = w;
 canvas.height = h;
 
-const player = {
-  x: 250,
-  y: 600,
+class Player {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.w = 40;
+    this.h = 40;
+  }
+}
+
+player1 = new Player();
+player1 = {
+  x: 20,
+  y: 20,
   w: 40,
-  h: 60,
-  score: 0,
+  h: 30,
 };
 
 class Object {
@@ -22,12 +37,12 @@ class Object {
   }
 }
 
-let collectible = new Object();
+let collectible1 = new Object();
 
 let obstacleArr = [];
 
 const surfer = new Image();
-car.src = "images/car.png";
-car.onload = function () {
-  ctx.drawImage(surfer, player.x, player.y, player.w, player.h);
+surfer.src = "images/surfing-gal-player.png";
+surfer.onload = function () {
+  ctx.drawImage(surfer, player1.x, player1.y, player1.w, player1.h);
 };
